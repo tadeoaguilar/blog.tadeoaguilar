645af708ebf8d20945f4bb2af2a660f275be75c5
Tadeo Aguilar <Tadeo.Aguilar@FINNING.COM>
2022-08-03 18:00:46 -0500

    svg

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,16 @@
 import Link from 'next/link'
 import Image from 'next/image'
-
+import Logo from './Logo'
 export default function Header() {
   return (
     <header className='bg-gray-900 text-gray-100 shadow w-full'>
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
-        <Link href='/'>
+        
           <a className='flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
-            <Image src='/images/logo.png' width={40} height={40} alt='logo' />
+           <Logo></Logo>
             <span className='ml-3 text-xl'>DevSpace</span>
           </a>
-        </Link>
+        
         <nav className='flex flex-wrap md:w-4/5 items-center justify-end text-base md:ml-auto'>
           <Link href='/blog'>
             <a className='mx-5 cursor-pointer uppercase hover:text-indigo-300'>
---
+++ components/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>
+    <svg width="361" height="361" viewBox="0 0 361 361"  xmlns="http://www.w3.org/2000/svg">
+        <g id="blog-logo">
+        <path className='test' id="bg" d="M360.57 0.420044H0.349976V360.4H360.57V0.420044Z" fill="#2F1123"/>
+        
+            <path id="purple-right" d="M305.83 180.41C305.83 108.72 249.62 50.6 180.28 50.6V310.21C249.62 310.21 305.83 252.09 305.83 180.4V180.41Z" fill="#9E1F63"/>
+        
+        <path id="black-left" d="M54.73 180.41C54.73 108.72 110.94 50.6 180.28 50.6V310.21C110.94 310.21 54.73 252.09 54.73 180.4V180.41Z" fill="#231F20"/>
+       
+        <path id="purple-left" d="M117.51 180.41C117.51 216.25 145.61 245.31 180.28 245.31V115.5C145.61 115.5 117.51 144.56 117.51 180.4V180.41Z" fill="#9E1F63"/>
+        <path id="black-right" d="M243.06 180.41C243.06 216.25 214.96 245.31 180.29 245.31V115.5C214.96 115.5 243.06 144.56 243.06 180.4V180.41Z" fill="#231F20"/>
+        </g>
+</svg>
+</div>
+  )
+}
+
+export default Logo
+
+
